d1ee2c2bd1d18c84570b4e0d428f3d6dd7ec9bdd
Steve Geluso <stevegeluso@gmail.com>
2017-01-29 18:27:20 -0800

    converting manual templates to use Django generic views

--- polls/views.py
+++ polls/views.py
@@ -1,27 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question
 
 # Create your views here.
-def index(request):
-    latest_questions = Question.objects.order_by('-pub_date')[:5]
-    context = {
-      'latest_question_list': latest_questions
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
     
-def details(request, question_id):
-    try:
-      question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-      raise Http404("Question does not exist")
-    return render(request, 'polls/details.html', {'question': question})
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
     
-def results(request, question_id):
-    question = Question.objects.get(pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class DetailsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/details.html'
+    
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
     
 def vote(request, question_id):
     question = Question.objects.get(pk=question_id)
