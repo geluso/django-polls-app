3949f4211aa5ec59408b03748f0cbbcb137ab8b8
Steve Geluso <stevegeluso@gmail.com>
2017-01-29 17:42:21 -0800

    rendering basic text to view route

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello.")
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    questions = []
+    for question in latest_questions:
+      questions.append(question.question_text)
+    text = ", ".join(questions)
+    return HttpResponse(text)
     
 def details(request, question_id):
     return HttpResponse(f"You're looking at question {question_id}")
