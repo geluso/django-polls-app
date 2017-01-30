4f9941238d2ef6246e0d0771b8014381d2e07d04
Steve Geluso <stevegeluso@gmail.com>
2017-01-29 17:37:51 -0800

    adding more routes with URL params

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello.")
+    
+def details(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+    
+def results(request, question_id):
+    return HttpResponse(f"You're looking at results for question {question_id}")
+    
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
