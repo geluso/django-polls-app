f9ba5674f5ca3ad2ce8deaeddb52d145be590984
Steve Geluso <stevegeluso@gmail.com>
2017-01-29 18:50:05 -0800

    adding choices so they display in the admin panel with Questions

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+# StackedInline displays the Choices taking up more space.
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+  # specifically defining fields here imposes an order on how
+  # the fields are display in the admin panel
+  # defining only fields only imposes an order on fields.
+  # fields = ['pub_date', 'question_text']
+  # defining field sets groups different fields into sections on the page.
+  fieldsets = [
+    (None, {'fields': ['question_text']}),
+    ('Date Information', {'fields': ['pub_date']})
+  ]
+  inlines = [ChoiceInline]
+
+# We're registering the Question model to display with the
+# configured QuestionAdmin class.
+admin.site.register(Question, QuestionAdmin)
 
 # Register your models here.
