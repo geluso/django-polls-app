5cfb9227fdb22b044b90680b1961d6c52cc4be5f
Steve Geluso <stevegeluso@gmail.com>
2017-01-29 18:17:29 -0800

    hooking up vote form and results view

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.urls import reverse
 
 from .models import Question
 
@@ -19,7 +20,15 @@ def details(request, question_id):
     return render(request, 'polls/details.html', {'question': question})
     
 def results(request, question_id):
-    return HttpResponse(f"You're looking at results for question {question_id}")
+    question = Question.objects.get(pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
     
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}")
+    question = Question.objects.get(pk=question_id)
+    choice_id = request.POST["choice"]
+    
+    choice = question.choice_set.get(pk=choice_id)
+    choice.votes += 1
+    choice.save()
+    
+    return HttpResponseRedirect(reverse('results', args=(question.id,)))
